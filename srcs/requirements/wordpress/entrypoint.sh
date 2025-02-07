f6147a1bbc16e642ca807cc92e57da414a20ac5b
smclacke <smclacke@wormy.myguest.virtualbox.org>
2025-02-07 17:43:57 +0100

    better testing environment but wordpress wont connect with mariadb...

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -23,28 +23,28 @@ else
 
 	cd /var/www/html
 	wp config create --path="/var/www/html" \
-					 --dbname="${WORDPRESS_DB_NAME}" \
-					 --dbuser="${WORDPRESS_DB_USER}" \
-					 --dbpass="${WORDPRESS_DB_PASSWORD}" \
-					 --dbhost="${WORDPRESS_DB_HOST}" \
+					 --dbname="$WORDPRESS_DB_NAME" \
+					 --dbuser="$WORDPRESS_DB_USER" \
+					 --dbpass="$WORDPRESS_DB_PASSWORD" \
+					 --dbhost="$WORDPRESS_DB_HOST" \
 					 --allow-root
 	
 	echo "installing wordpress..."
 
 	wp core install --path="/var/www/html" \
 					--title="inception" \
-					--admin_user="${WORDPRESS_ADMIN_NAME}" \
-					--admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
-					--admin_email="${WORDPRESS_ADMIN_EMAIL}" \
-					--url="${DOMAIN_NAME}" \
+					--admin_user="$WORDPRESS_ADMIN_NAME" \
+					--admin_password="$WORDPRESS_ADMIN_PASSWORD" \
+					--admin_email="$WORDPRESS_ADMIN_EMAIL" \
+					--url="$DOMAIN_NAME" \
 					--skip-email \
 					--allow-root
 
 	echo "creating wp user..."
 
-	wp user create "${WORDPRESS_USER_NAME}" user@user.com \
+	wp user create "$WORDPRESS_USER_NAME" user@user.com \
 					 --path="/var/www/html" \
-					 --user_password="${WORDPRESS_USER_PASSWORD}" \
+					 --user_password="$WORDPRESS_USER_PASSWORD" \
 					 --allow-root
 fi
 
